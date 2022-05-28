7d01048a9d4d33149227396896baa366689f4b14
iaksharhate <iaksharhate@gmail.com>
2022-05-29 00:01:20 +0530

    Added: Refactored the code with function.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,6 +1,6 @@
 console.log("Welcome to Employee wage program.");
 
-//UC 2
+//UC 3
 const IS_ABSENT = 0;
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
@@ -8,23 +8,20 @@ const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
 const WAGE_PER_HOUR = 20;
 
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_ABSENT:
+            return 0;
+        
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+    }
+}
 let empHours = 0;
 let empCheck = Math.floor(Math.random()*10)%3;
-
-switch (empCheck){
-    case IS_ABSENT:
-        empHours = 0
-        console.log("Employee is ABSENT.");
-        break;
-    
-    case IS_FULL_TIME:
-        empHours = FULL_TIME_HOURS;
-        break;
-    
-    case IS_PART_TIME:
-        empHours = PART_TIME_HOURS;
-        break;
-}
-
+empHours = getWorkingHours(empCheck);
 let empWage = empHours * WAGE_PER_HOUR;
 console.log("Employee wage: "+ empWage);
