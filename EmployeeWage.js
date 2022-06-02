2243529663740661b83f5d0fe1f6b171ee190e32
iaksharhate <iaksharhate@gmail.com>
2022-06-02 11:51:33 +0530

    Added: Ability to store daily wage, hours and day in single object.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,6 +1,6 @@
 console.log("Welcome to Employee wage program.");
 
-//UC 9
+//UC 10
 const IS_ABSENT = 0;
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
@@ -26,9 +26,7 @@ function getWorkingHours(empCheck){
 let empHours = 0;
 let totalWorkingHours = 0;
 let totalWorkingDays = 0;
-let empDailyWageArray = new Array();
-let empDailyWageMap = new Map();
-let empDailyHoursMap = new Map();
+let empDailyHoursWageArray = new Array();
 
 function calculateDailyWage(empHours){
     return empHours * WAGE_PER_HOUR;
@@ -39,37 +37,17 @@ while (totalWorkingDays < MAX_DAYS_IN_MONTH && totalWorkingHours < MAX_HRS_IN_MO
     let empCheck = Math.floor(Math.random()*10)%3;
     empHours = getWorkingHours(empCheck);
     totalWorkingHours = totalWorkingHours + empHours;
-    empDailyWageArray.push(calculateDailyWage(empHours))  //Storing daily wage in array.
-    empDailyWageMap.set(totalWorkingDays, calculateDailyWage(empHours)); //Storing daily wage in a map.
-    empDailyHoursMap.set(totalWorkingDays, empHours);
+    empDailyHoursWageArray.push({
+        dayNum: totalWorkingDays,
+        dailyHours: empHours,
+        dailyWage: calculateDailyWage(empHours),
+        toString(){
+            return '\nDay'+ this.dayNum + ' => Working hours is '+ this.dailyHours + ' and wage earned = ' + this.dailyWage
+        },
+    })
 }
 let empWage = calculateDailyWage(totalWorkingHours);
-console.log("Total Days: "+totalWorkingDays +" Total Hours: "+totalWorkingHours+ " Employee wage: "+ empWage);
-console.log("******************************************************************");
 
-// UC9
-
-const findTotal = (totalVal, dailyVal) => {
-    return totalVal + dailyVal;
-}
-let count = 0;
-let totalHours = Array.from(empDailyHoursMap.values()).reduce(findTotal, 0);
-let totalSalary = empDailyWageArray.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
-
-console.log("UC9 A: Employee wage with Arrow "+"Total hours: "+ totalHours +" Total wages: "+ totalSalary);
-
-let nonWorkingDays = new Array();
-let partWorkingDays = new Array();
-let fullWorkingDays = new Array();
-
-empDailyHoursMap.forEach( (value, key, map) => {
-    if (value == 8) fullWorkingDays.push(key);
-    else if (value == 4) partWorkingDays.push(key);
-    else nonWorkingDays.push(key);
-});
-
-console.log("Full working days: "+ fullWorkingDays);
-console.log("Part working days: "+ partWorkingDays);
-console.log("Non working days: "+ nonWorkingDays);
-console.log("******************************************************************");
+console.log("UC 10: Showing daily hours worked and wage earned: "+ empDailyHoursWageArray);
+console.log("Total wage earned: "+ empWage);
 console.log("******************************************************************");
